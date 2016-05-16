40e11f203ce2a892468176452ef8b3ffdfc2f59d
Wendy Huang <yhuang10@lakeheadu.ca>
2016-05-16 16:28:49 -0400

    adding files

---
+++ app.js
@@ -0,0 +1,99 @@
+// 加载依赖库，原来这个类库都封装在connect中，现在需地注单独加载
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+var session = require('express-session');
+
+// 加载路由控制
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+global.dbHandel = require('./database/dbHandel');
+global.db = mongoose.connect("mongodb://localhost:27017/nodedb");
+
+// 创建项目实例
+var app = express();
+app.use(session({ 
+  secret: 'secret',
+  cookie:{ 
+    maxAge: 1000*60*30
+  }
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine("html",require("ejs").__express); // or   app.engine("html",require("ejs").renderFile);
+//app.set("view engine","html");
+app.set('view engine', 'html');
+
+// 定义icon图标
+app.use(favicon(__dirname + '/public/images/favicon.jpg'));
+// 定义日志和输出级别
+app.use(logger('dev'));
+// 定义数据解析器
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+// 定义cookie解析器
+app.use(cookieParser());
+// 定义静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){ 
+  res.locals.user = req.session.user;
+  var err = req.session.error;
+  delete req.session.error;
+  res.locals.message = "";
+  if(err){ 
+    res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+  }
+  next();
+});
+
+// 匹配路径和路由
+app.use('/', routes);  // 即为为路径 / 设置路由
+app.use('/users', users); // 即为为路径 /users 设置路由
+app.use('/login',routes); // 即为为路径 /login 设置路由
+app.use('/register',routes); // 即为为路径 /register 设置路由
+app.use('/home',routes); // 即为为路径 /home 设置路由
+app.use("/logout",routes); // 即为为路径 /logout 设置路由
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// 输出模型app
+module.exports = app;
