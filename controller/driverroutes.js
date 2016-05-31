e97e1396e1edfa0235007e13299e80de02f59221
Wendy Huang <yhuang10@lakeheadu.ca>
2016-05-31 16:23:18 -0400

    find ok but not showing up on the page

--- controller/driverroutes.js
+++ controller/driverroutes.js
@@ -6,12 +6,13 @@ exports.driverroutes = function() {
             res.redirect("/login"); //未登录则重定向到 /login 路径
         } else {
             var Route = global.dbHandel.getModel('driverroute');
-            // Route.find(req.session.user, function(userId, callback) {
+            // Route.statics.getModel(req.session.user, function(userId, callback) {
             //     this.model('driverroute').find({ name: userId }, callback);
             // })
-            Route.find(req.session.user, function(err, doc) {
+            Route.find({name: req.session.user}, function(err, doc) {
+                    req.session.user = doc;
                     console.log(doc);
-                    res.render("driverRoutes", { title: 'ShareCar' }, { driverroutes: doc });
+                    res.render("driverRoutes", { driverroutes: doc });
                 }
 
 
