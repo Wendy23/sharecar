d3eedb0a5af0a47c663f72fd6d4ca08859af1a61
Wendy Huang <yhuang10@lakeheadu.ca>
2016-09-01 00:51:44 +0800

    done messages I sent (prepare to do messages I received, lack of status)

--- controller/myMessage.js
+++ controller/myMessage.js
@@ -8,36 +8,70 @@ exports.myMessage = function () {
             var Message = global.dbHandel.getModel('driverroute');
             var name = req.session.user;
             var name1 = name._id;
-            Message.find({'riderid.userid':req.session.user._id}, function(err, doc) {
-                //req.session.user = doc;
-                console.log("doc",doc);
-                res.render("myMessage", {driverroutes: doc,title: 'ShareCar'});
-                //res.render("myProfile", { user: JSON.stringify(doc), title: 'ShareCar' });
-            });
+            Message.aggregate(
+                {
+                    $unwind: '$riderid'
+                },
+                {
+                    $match: {'riderid.userid': req.session.user._id}
+                },
+                //{
+                //    $project: {cost: 1, riderid: 1, _id: 0, passnum: 1}
+                //},
+                //{
+                //    "$group": {"_id": "$_id", "riderid": {'$push': "$riderid"}}
+                //},
+                function (err, doc) {
+                    //req.session.user = doc;
+                    console.log("doc", doc);
+                    res.render("myMessage", {driverroutes: doc, title: 'ShareCar'});
+                    //res.render("myProfile", { user: JSON.stringify(doc), title: 'ShareCar' });
+                }
+            );
         }
     }
 }
 
 
-
 exports.removeRoute = function () {
     return function (req, res) {
-        console.log("get into remove route");
+        console.log("get into remove riderId");
         if (!req.session.user) { //到达/home路径首先判断是否已经登录
             req.session.error = "请先登录"
             res.redirect("/login"); //未登录则重定向到 /login 路径
         } else {
             console.log(req.body, req.query);
             var Route = global.dbHandel.getModel('driverroute');
+            var userid = req.session.user._id;
             var currentId = req.body.routeId;
             var passnum = req.body.passenger;
-            Route.update({_id: currentId}, {$addToSet: {"riderid":{"userid": req.session.user._id}}}, {}, function (err, raw) {
-                Route.update({riderid:{userid:req.session.user._id}},{riderid:{panum:passnum}}, {$inc: {occupied: passnum}}, {}, function (err, raw) {
-                    console.log("update riderid", err, raw);
+            Route.update(
+                {
+                    "_id": currentId,
+                    "riderid.userid": userid
+                },
+                {
+                    "$inc": {
+                        'riderid.$.passnum': -passnum,
+                        'occupied': -passnum
+                    }
+                },
+                {}, function (err, raw) {
+                    console.log("raw: ", raw);
+                    Route.update(
+                        {
+                            '_id': currentId
+                        },
+                        {
+                            $pull: {
+                                "riderid": {passnum: 0}
+                            }
+                        }, {}, function (err, raw) {
+                        }
+                    )
                     if (err) res.status(500);
                     res.send(raw);
-                });
-            });
+                })
         }
     }
 }
