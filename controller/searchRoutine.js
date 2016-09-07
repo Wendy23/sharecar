0b0e870e12140e1304ba02bc8800bcf784400868
Wendy Huang <yhuang10@lakeheadu.ca>
2016-09-07 13:45:09 +0800

    prepare to do routine (search and choose)

--- controller/searchRoutine.js
+++ controller/searchRoutine.js
@@ -1,12 +1,17 @@
-exports.routine = function() {
+exports.routine = function () {
     return function (req, res) {
-        console.log("get into driverrouters");
+        console.log("get into routine");
         if (!req.session.user) { //到达/home路径首先判断是否已经登录
             req.session.error = "请先登录"
             res.redirect("/login"); //未登录则重定向到 /login 路径
         } else {
-            res.render("searchRoutine", {});
+            var Routine = global.dbHandel.getModel('routine');
+            console.log(req.query);
+            Routine.find({}, function (err, doc) {
+                console.log(doc);
+                res.render("searchRoutine", {routine: doc});
 
+            })
         }
     }
 }
