84da3ef35de04869e83a7130326a562a442f6b3c
Wendy Huang <yhuang10@lakeheadu.ca>
2016-09-30 14:24:53 -0400

    done editroute, done two days, 5 left map done prepare do duplicate(search side) map and show info

--- public/js/project.js
+++ public/js/project.js
@@ -8,12 +8,16 @@ function initMap() {
 
 
 // Create the search box and link it to the UI element.
-    var input = document.getElementById('PostalCode');
+    var input = document.getElementById('from');
     var searchBox = new google.maps.places.SearchBox(input);
+
+    var input2 = document.getElementById('to');
+    var searchBox2 = new google.maps.places.SearchBox(input2);
 
 // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
+        searchBox2.setBounds(map.getBounds());
     });
 
     var markers = [];
@@ -26,14 +30,21 @@ function initMap() {
             return;
         }
 
-        // Clear out the old markers.
-        markers.forEach(function (marker) {
-            marker.setMap(null);
-        });
-        markers = [];
+        //// Clear out the old markers.
+        //markers.forEach(function (marker) {
+        //    marker.setMap(null);
+        //});
+        //markers = [];
+
+        // Fit the bounds within in the map
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < markers.length; i++) {
+            console.log(markers.length);
+            bounds.extend(markers[i].getPosition());
+        }
+        map.fitBounds(bounds);
 
         // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
         places.forEach(function (place) {
             var icon = {
                 url: place.icon,
@@ -50,6 +61,75 @@ function initMap() {
                 title: place.name,
                 position: place.geometry.location
             }));
+
+            console.log("place" + place.formatted_address);
+
+            var reqResult = place.formatted_address;
+            var getAreaInfo = reqResult.substring(reqResult.indexOf(",") + 1);
+            console.log("geuwi" + getAreaInfo);
+
+            var ta = document.getElementById('PostalCode');
+            ta.value = getAreaInfo;
+
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
+    searchBox2.addListener('places_changed', function () {
+        var places = searchBox2.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        //// Clear out the old markers.
+        //markers.forEach(function (marker) {
+        //    marker.setMap(null);
+        //});
+        //markers = [];
+
+        // Fit the bounds within in the map
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < markers.length; i++) {
+            console.log(markers.length);
+            bounds.extend(markers[i].getPosition());
+        }
+        map.fitBounds(bounds);
+
+        // For each place, get the icon, name and location.
+        places.forEach(function (place) {
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            console.log("place" + place.formatted_address);
+
+            var reqResult = place.formatted_address;
+            var getAreaInfo = reqResult.substring(reqResult.indexOf(",") + 1);
+            console.log("geuwi" + getAreaInfo);
+
+            var ta = document.getElementById('PostalCode2');
+            ta.value = getAreaInfo;
+
 
             if (place.geometry.viewport) {
                 // Only geocodes have viewport.
